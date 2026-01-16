b7fa281e0ef802bab35751f30de3d02a14948dca
auth1ery <skunkolee@gmail.com>
2026-01-16 07:49:11 -0700

    fixesssss

--- server.js
+++ server.js
@@ -582,16 +582,25 @@ app.post("/api/rng-link", async (req, res) => {
     return res.status(400).json({ error: "Missing data" })
   }
 
-  if (!pendingTokens[token]) {
-    return res.status(400).json({ error: "Invalid or expired token" })
-  }
+  const result = await pool.query(
+  "SELECT * FROM rng_tokens WHERE token = $1",
+  [token]
+)
+
+if (result.rows.length === 0) {
+  return res.status(400).json({ error: "Invalid or expired token" })
+}
 
   await pool.query(
     "UPDATE users SET rng_linked = TRUE WHERE nickname = $1",
     [nickname.toLowerCase()]
   )
 
-  delete pendingTokens[token]
+  await pool.query(
+  "DELETE FROM rng_tokens WHERE token = $1",
+  [token]
+)
+
 
   res.sendStatus(200)
 })
